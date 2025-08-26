eda5dbe9de27ecc7fd619a66ee28e4ff746f2e12
Sourabh Kulkarni <rockylearnings@gmail.com>
2025-08-26 12:59:40 +0530

    Remove Terraform .tf files (to re-add cleanly)

--- skeleton/terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "cloudfront_domain_name" {
-  description = "The domain name of the CloudFront distribution."
-  value       = aws_cloudfront_distribution.s3_distribution.domain_name
-}
-
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket."
-  value       = aws_s3_bucket.website_bucket.id
-}
