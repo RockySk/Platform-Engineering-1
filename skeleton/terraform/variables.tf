ad694a090c176185d9c112b76831502331c59421
Sourabh Kulkarni <rockylearnings@gmail.com>
2025-08-26 13:00:20 +0530

    Re-add Terraform IaC for S3 + CloudFront Backstage demo app

---
+++ skeleton/terraform/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "The AWS region to deploy resources in."
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "bucket_name" {
+  description = "The unique name for the S3 bucket."
+  type        = string
+}
